127cebfaac175fb969149c5c199e9c687388a939
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-01-19 15:18:00 +0100

    function for light on

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById("accendi").addEventListener("click", function() {
+    let lampadina = document.getElementById("lampadina");
+    lampadina.src = "img-5/yellow_lamp.png" ;
+} ) ;
